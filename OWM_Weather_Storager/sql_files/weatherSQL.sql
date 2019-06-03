a180d5a22a0f3515c141af8f4f0c5d17651584c7
Marcel Verst <ma-verst@gmx.de>
2019-06-03 16:44:19 +0200

    Changed sql file

--- OWM_Weather_Storager/sql_files/weatherSQL.sql
+++ OWM_Weather_Storager/sql_files/weatherSQL.sql
@@ -1,2 +1,3 @@
+CREATE DATABASE weather;
 CREATE TABLE location(cityID INT NOT NULL, lon DOUBLE, lat DOUBLE, cityName VARCHAR(255), PRIMARY KEY (cityID));
 CREATE TABLE weatherConditions(id INT NOT NULL AUTO_INCREMENT, recordedTime INT, temperature DOUBLE, pressure INT, humidity INT, min_temperature DOUBLE, max_temperature DOUBLE, cityID INT, PRIMARY KEY (id), FOREIGN KEY(cityID) REFERENCES location(cityID));
