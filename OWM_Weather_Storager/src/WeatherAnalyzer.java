a01aba3f2b4b2e449ce7b75ba8491d0d652142f8
Marcel Verst <ma-verst@gmx.de>
2019-06-16 15:10:11 +0200

    Added hibernate construct including employee example with CRUD ops

--- OWM_Weather_Storager/src/WeatherAnalyzer.java
+++ OWM_Weather_Storager/src/WeatherAnalyzer.java
@@ -1,9 +1,17 @@
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.json.simple.JSONObject;
 
 import dao.DBHandler;
+import dao.Employee;
 import dao.LocationEntry;
 import dao.WeatherConditionsEntry;
-import plotting.HeatMapPainter;
 
 /**
  * The WeatherAnalyzer serves as the entry point of the application. Here you have to set your API_KEY first.
@@ -20,11 +28,22 @@ import plotting.HeatMapPainter;
  */
 public class WeatherAnalyzer {
 	private static String API_KEY = "";
+	private static SessionFactory factory;
 
 	public static void main(String[] args) {
-		HeatMapPainter demo = new HeatMapPainter();
-		demo.pack();
-		demo.setVisible(true);
+		try {
+			factory = new Configuration().configure().buildSessionFactory();
+		} catch(Throwable ex) {
+			System.err.println("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+		addEmployee("Marcel", "Verst", 50000);
+		System.out.println("Added new Employee");
+		listEmployees();
+
+		//		HeatMapPainter demo = new HeatMapPainter();
+		//		demo.pack();
+		//		demo.setVisible(true);
 
 		//		SwingGUIExample myGui = new SwingGUIExample();
 		//		myGui.drawWindow();
@@ -45,6 +64,96 @@ public class WeatherAnalyzer {
 		//		} catch (IOException e) {
 		//			e.printStackTrace();
 		//		}
+	}
+
+	/**
+	 * Creates an Employee object with the given parameters and stores the object into the employee table
+	 * 
+	 * @param String The first name
+	 * @param String The last name
+	 * @param Integer The salary
+	 */
+	public static void addEmployee(String fname, String lname, int salary) {
+		Session session = factory.openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			Employee employee = new Employee(fname, lname, salary);
+			session.save(employee); 
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace(); 
+		} finally {
+			session.close(); 
+		}
+	}
+
+	/**
+	 * Prints all employee entries of the table.
+	 */
+	public static void listEmployees(){
+		Session session = factory.openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			List employees = session.createQuery("FROM Employee").list(); 
+			for (Iterator iterator = employees.iterator(); iterator.hasNext();){
+				Employee employee = (Employee) iterator.next(); 
+				System.out.print("First Name: " + employee.getFirstName()); 
+				System.out.print("  Last Name: " + employee.getLastName()); 
+				System.out.println("  Salary: " + employee.getSalary()); 
+			}
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace(); 
+		} finally {
+			session.close(); 
+		}
+	}
+
+	/**
+	 * Update the salary of an employee based on its ID.
+	 * 
+	 * @param Integer The ID of the employee
+	 * @param Integer The new salary
+	 */
+	public void updateEmployee(Integer EmployeeID, int salary ){
+		Session session = factory.openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			Employee employee = (Employee)session.get(Employee.class, EmployeeID); 
+			employee.setSalary( salary );
+			session.update(employee); 
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace(); 
+		} finally {
+			session.close(); 
+		}
+	}
+
+	public void deleteEmployee(Integer EmployeeID){
+		Session session = factory.openSession();
+		Transaction tx = null;
+
+		try {
+			tx = session.beginTransaction();
+			Employee employee = (Employee)session.get(Employee.class, EmployeeID); 
+			session.delete(employee); 
+			tx.commit();
+		} catch (HibernateException e) {
+			if (tx!=null) tx.rollback();
+			e.printStackTrace(); 
+		} finally {
+			session.close(); 
+		}
 	}
 
 	/**
