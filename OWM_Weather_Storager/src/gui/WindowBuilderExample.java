4ae0aa8e5bc27d0cd293e11bbea549c1a5f6b642
Marcel Verst <ma-verst@gmx.de>
2019-06-17 15:56:29 +0200

    Organised imports

--- OWM_Weather_Storager/src/gui/WindowBuilderExample.java
+++ OWM_Weather_Storager/src/gui/WindowBuilderExample.java
@@ -1,26 +1,15 @@
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.GridLayout;
-import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JMenuBar;
-import javax.swing.JLabel;
 
 public class WindowBuilderExample extends JFrame {
 
