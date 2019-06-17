8db62781fdd7e59c6bc9595ee88f92291f24810c
Marcel Verst <ma-verst@gmx.de>
2019-06-17 15:56:59 +0200

    Added default serial version id

--- OWM_Weather_Storager/src/gui/WindowBuilderExample.java
+++ OWM_Weather_Storager/src/gui/WindowBuilderExample.java
@@ -13,6 +13,10 @@ import javax.swing.border.EmptyBorder;
 
 public class WindowBuilderExample extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
